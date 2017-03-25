522043c68ae22fcc60a44f09ee9c725485a93b78
zhangyue_githug <845680003@qq.com>
2017-03-25 10:39:07 +0800

    Initial Commit

---
+++ RZDynamicCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RZDynamicCollectionView
+//
+//  Created by zy on 2017/3/25.
+//  Copyright © 2017年 renwohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
