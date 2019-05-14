251e33cc835bbb93313858fa24a371e930269cb8
aria.zhang <aria.zhang@alo7.com>
2019-05-14 18:15:47 +0800

    add file

---
+++ RZDynamicCollectionView/GitTest.h
@@ -0,0 +1,17 @@
+//
+//  GitTest.h
+//  RZDynamicCollectionView
+//
+//  Created by saybot on 2019/5/14.
+//  Copyright © 2019 renwohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GitTest : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RZDynamicCollectionView/GitTest.m
@@ -0,0 +1,21 @@
+//
+//  GitTest.m
+//  RZDynamicCollectionView
+//
+//  Created by saybot on 2019/5/14.
+//  Copyright © 2019 renwohua. All rights reserved.
+//
+
+#import "GitTest.h"
+
+@implementation GitTest
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
